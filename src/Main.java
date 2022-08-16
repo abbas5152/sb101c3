4130c7d59696c7ef50970413d47b89b502beacca
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-08-16 16:48:57 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        StudentDb s1=new StudentDb();
+        ss o=new ss(1,"abs","jam","abc","123");
+        s1.map.put(o,"one");
+
+        ss t=new ss(2,"abs","jam","abc","123");
+        s1.map.put(t,"on");
+        System.out.println(s1);
+
+
+
+
+
+    }
+}
