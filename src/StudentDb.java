cfdf39be842981e062e11f15c272b2c4d145528c
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-08-16 16:39:49 +0530

    Update StudentDb.java

--- src/StudentDb.java
+++ src/StudentDb.java
@@ -1,3 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class StudentDb {
-    
+
+    Map<ss,String> map=new HashMap<>();
+
+
+    void addStudent(ss student){
+
+       student.studentId=1;
+       student.studentName="Abbas";
+       student.student_email="abc";
+       student.studentAddress="jam";
+       student.studentPassword="123";
+
+        student.studentId=2;
+        student.studentName="Abb";
+        student.student_email="abc";
+        student.studentAddress="jam";
+        student.studentPassword="123";
+
+    }
+    Set<Map.Entry<ss,String>> studentsData=map.entrySet();
 }
+
